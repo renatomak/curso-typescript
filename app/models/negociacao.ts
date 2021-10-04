88623bc7eccf14f330fc582b7c1b44f1af13d694
renatomak <renato.mark.silva@gmail.com>
2021-10-04 11:16:48 -0300

    first commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,15 @@
+export class Negociacao {
+  constructor(
+    private _data: Date,
+    private readonly quantidade: number,
+    private readonly valor: number
+  ) {}
+
+  get data(): Date {
+      const data = new Date(this._data.getTime());
+    return data;
+  }
+  get volume(): number {
+      return this.quantidade * this.valor;
+  }
+}
